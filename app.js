e3952eb04242dc8f905233c31bb847ffbb5acb8e
Taieb <mohamedtaieb.alfalah@esprit.tn>
2018-03-03 17:31:24 +0100

    👨‍💻

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 //Map Global Promise 
@@ -12,6 +12,10 @@ mongoose.connect('mongodb://localhost/taieb-dev')
 .then(()=>console.log('MongoDb Connected ...'))
 .catch(err =>console.log(err));
 
+//Load Ideas Model
+
+require('./models/Ideas');
+const Idea = mongoose.model('ideas');
 
 
 	
@@ -20,6 +24,11 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+//BodyParser
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 
 // Index Route
@@ -37,6 +46,74 @@ app.get('/about', (req, res) => {
   	title:title
   });
 });
+
+//Idea Index Page
+app.get('/ideas',(req,res)=>{
+	Idea.find({})
+	.sort({date:'desc'})
+	.then(ideas=>{
+		res.render('ideas/index',{
+			ideas:ideas
+		});
+	});
+
+});
+
+
+
+//Add Idea Form
+
+app.get('/ideas/add',(req,res)=>{
+	res.render('ideas/add');
+});
+
+
+// Edit Idea Form
+
+app.get('/ideas/edit/:id',(req,res)=>{
+	Idea.findOne({
+		_id:req.params.id
+	})
+	.then(idea=>{
+			res.render('ideas/edit',{
+		idea:idea
+	});
+	});
+
+});
+
+
+
+//Procss Form
+
+app.post('/ideas',(req,res)=>{
+let errors=[];
+if (!req.body.title){
+	errors.push({text:'Please Add a title'});
+}
+if (!req.body.details){
+	errors.push({text:'Please add some details'});
+}
+if(errors.length>0){
+	res.render('ideas/add',{
+	errors:errors,
+	title:req.body.title,
+	details:req.body.details
+	});
+   } else{
+   	const newUser = {
+   		title:req.body.title,
+   		details:req.body.details
+   	}
+  new Idea(newUser)
+  		.save()
+  		.then(idea=>{
+  			res.redirect('/ideas');
+  		})
+   }
+});
+
+
 
 const port = 3000;
 
--- models/ideas.js
+++ models/ideas.js
@@ -2,3 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Creation du Schéma
+
+const IdeaSchema = new Schema({
+title:{
+	type:String,
+	required:true
+},
+details:{
+	type:String,
+	required:true
+},
+date:{
+	type:Date,
+	default:Date.now //par Défaut
+}
+});
+
+mongoose.model('ideas',IdeaSchema)
